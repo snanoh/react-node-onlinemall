abd3559da20c270c14634668b47a1f0eee1ed40a
User <njs1324@gmail.com>
2021-03-29 22:36:37 +0900

    20210329 commit

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,12 +1,15 @@
 import React, { useEffect,useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { getCartItems } from '../../../_actions/user_actions'
+import { getCartItems, removeCartItem } from '../../../_actions/user_actions'
 import UserCardBlock from './Sections/UserCardBlock'
+import { Empty } from 'antd';
 
 function CartPage(props) {
     const dispatch = useDispatch()
 
     const [Total, setTotal] = useState(0)
+    const [ShowTotal, setShowTotal] = useState(false)
+
 
     useEffect(() => {
 
@@ -34,17 +37,37 @@ function CartPage(props) {
         })
 
         setTotal(total)
+        setShowTotal(true)
+    }
+
+    let removeFromCart = (productId) => {
+        dispatch(removeCartItem(productId))
+        .then(response => {
+            if(response.payload.productInfo.length <= 0){
+                setShowTotal(false)
+            }
+        })
+
     }
 
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
             <h1>My Cart</h1>
             <div>
-                <UserCardBlock products={props.user.cartDetail}/>
+                <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart} />
             </div>
+
+        {ShowTotal ?
+
             <div style={{ marginTop: '3rem' }}>
                 <h2>Total Amount : ${Total}</h2>
             </div>
+            :
+            <>
+            <br />
+            <Empty description={false} />
+            </>
+            }
         </div>
     )
 }
